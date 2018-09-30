638bfd628227fc858178554e5203d62735e6c63f
Kane Shih <kaneshih@yahoo-inc.com>
2018-09-30 16:07:46 +0800

    MainActivity - addItem method

--- app/src/main/java/tw/kaneshih/kanetest/ui/MainActivity.kt
+++ app/src/main/java/tw/kaneshih/kanetest/ui/MainActivity.kt
@@ -2,10 +2,12 @@ package tw.kaneshih.kanetest.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.core.view.setPadding
+import tw.kaneshih.base.setSelectableItemBackground
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,29 +20,31 @@ class MainActivity : AppCompatActivity() {
                     ViewGroup.LayoutParams.MATCH_PARENT,
                     ViewGroup.LayoutParams.MATCH_PARENT)
 
-            addView(TextView(context).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT)
+            addItem {
                 text = "Click for Cards"
-                textSize = 48f
-                setPadding(32)
-                setOnClickListener {
-                    startActivity(ListActivity.getIntentForCards(context))
-                }
-            })
+                onClick { startActivity(ListActivity.getIntentForCards(context)) }
+            }
 
-            addView(TextView(context).apply {
-                layoutParams = ViewGroup.LayoutParams(
-                        ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT)
+            addItem {
                 text = "Click for Books"
-                textSize = 48f
-                setPadding(32)
-                setOnClickListener {
-                    startActivity(ListActivity.getIntentForBooks(context))
-                }
-            })
+                onClick { startActivity(ListActivity.getIntentForBooks(context)) }
+            }
         })
     }
+
+    private inline fun ViewGroup.addItem(init: TextView.() -> Unit) {
+        addView(TextView(context).apply {
+            layoutParams = ViewGroup.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT)
+            textSize = 48f
+            setPadding(32)
+            setSelectableItemBackground()
+
+            init()
+        })
+    }
+
+    fun View.onClick(action: View.() -> Unit) = setOnClickListener(action)
+
 }
